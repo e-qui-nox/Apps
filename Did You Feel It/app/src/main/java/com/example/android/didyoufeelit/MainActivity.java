de21515af3b639185d80bdc182a2d08b93ccea93
e-qui-nox <yamuna.pixie@gmail.com>
2018-09-02 11:37:31 +0530

    Updated MainActivity.java

--- Did You Feel It/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ Did You Feel It/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,16 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
     }
-
-    /**
-     * Update the UI with the given earthquake information.
-     */
     private void updateUi(Event earthquake) {
         TextView titleTextView = (TextView) findViewById(R.id.title);
         titleTextView.setText(earthquake.title);
@@ -53,5 +47,22 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
+        protected Event doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            Event result = Utils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+        protected void onPostExecute(Event result) {
+            if (result == null) {
+                return;
+            }
+
+            updateUi(result);
+        }
     }
 }
